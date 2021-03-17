71fd9bcee3bfa8da393b4fee5e9ac9715059810d
Jaimin Prajapati <75632224+Jaimin-dev21@users.noreply.github.com>
2021-03-17 13:51:10 -0400

    first commit

---
+++ compare.py
@@ -0,0 +1,14 @@
+# Compare Two CSV Files
+file_list = ['Sample1.csv', 'Sample2.csv']
+
+f1 = open(file_list[0], 'r').readlines()
+f2 = open(file_list[1], 'r').readlines()
+
+fName = open('Diffrence.csv', 'a')
+for _ in range(2):
+    for row in f1:
+        if row not in f2:
+            fName.write(row)
+    f1, f2 = f2, f1
+
+git commit -m "first commit"
